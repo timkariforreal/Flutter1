40c26151c542f65c63ef5a183488dddc55c182b3
timkariforreal <pavlid99@gmail.com>
2025-10-04 11:50:19 +0300

    Elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -37,14 +37,20 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child:
-        Text('ФИО - Каримов Тимур Манучехрович\nНомер группы - ИКБО-12-22\nНомер студенческого билета - 22Л0592\n ',
-        textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.red
-          ),
-        ),
+          child:
+          ElevatedButton(
+            onPressed: null,
+            style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.amber),
+                shape:
+                WidgetStateProperty.all(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                    )
+                )
+            ),
+            child: Text("Тестовая кнопка"),
+          )
       ),
     );
   }
